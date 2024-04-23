5618913690373f29ac8b327a5cea5927aaabe33b
syedasimrah14 <163674789+syedasimrah14@users.noreply.github.com>
2024-04-23 23:28:29 +0500

    Add files via upload
    Cli Based Currency Converter _Project 04

---
+++ index.ts
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const currencyData: any = {
+    USD: 1, //Base currency
+    EUR: 0.93,
+    GBP: 0.80,
+    PKR: 278.5,
+    NOK: 10.9,
+    SEK: 10.8,
+    DKK: 6.9,
+    INR: 83.2,
+};
+
+let userInput = await inquirer.prompt(
+    [
+    {
+        name: "amount",           
+        message: "Enter your amount",
+        type: "number",
+    },
+    {
+        name: "from",
+        message: "Enter from currency",
+        type: "list",
+        choices: ["USD", "NOK", "GBP", "PKR", "DKK", "SEK","EUR", "INR" ]
+
+    },
+    {
+        name: "to",           
+        message: "Enter to currency",
+        type: "list",
+        choices: ["USD", "NOK", "GBP", "PKR", "DKK", "SEK", "EUR" , "INR" ]
+    },
+   
+    ]
+);
+
+let fromCurrencyRate = currencyData[userInput.from];
+let toCurrencyRate = currencyData[userInput.to];
+let conversionAmount = userInput.amount;
+
+let baseAmount = userInput.amount/ fromCurrencyRate; //conversion to base currency
+let convertedAmount =Math.floor(baseAmount*toCurrencyRate);
+
+
+console.log("your converted amount is: " + convertedAmount + " " + userInput.to);
+
+
+
+   
